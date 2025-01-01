50b0ba762b9100222b9d3906ee22bef0d1cdf46e
Suthishma <suthishmababu@gmail.com>
2025-01-01 23:29:51 +0530

    First commit

---
+++ scripts/typingEffect.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function(){
+    new Typed('.typed',{
+        strings:['a Developer','a Designer'],
+        typeSpeed: 50,
+        backSpeed: 25,
+        backDelay: 2000,
+        loop:true,
+
+    })
+
+})
